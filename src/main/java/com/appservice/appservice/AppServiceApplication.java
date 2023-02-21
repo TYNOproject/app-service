209c22e0466a780f98086f98d6e64e647ab83adc
Omri Chen <omrichen70@gmail.com>
2023-02-21 17:40:58 +0200

    connection to db

--- src/main/java/com/appservice/appservice/AppServiceApplication.java
+++ src/main/java/com/appservice/appservice/AppServiceApplication.java
@@ -2,6 +2,7 @@ package com.appservice.appservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class AppServiceApplication {
