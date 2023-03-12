264c9e6625c615526eb8194b3e20f7de52daa71e
Omri Chen <omrichen70@gmail.com>
2023-03-12 13:52:08 +0200

    added changes

--- src/main/java/com/appservice/all/Postgres/ClassRepository.java
+++ src/main/java/com/appservice/all/Postgres/ClassRepository.java
@@ -4,9 +4,11 @@ import com.appservice.all.Entities.Class;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 import java.util.List;
 
 @Repository
 public interface ClassRepository extends JpaRepository<Class, Long> {
     List<Class> findAllByTeacherId(Long teacherId);
+    List<Class> findAllByStudentId(Long studentId);
 }
